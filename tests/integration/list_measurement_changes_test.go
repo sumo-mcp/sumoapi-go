8a84ea789ead7bacb744eb8516d812502f97fd94
Matheus Pimenta <matheuscscp@gmail.com>
2025-12-06 17:37:06 +0000

    Fix docs and tests for list rikishi changes APIs
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- tests/integration/list_measurement_changes_test.go
+++ tests/integration/list_measurement_changes_test.go
@@ -10,28 +10,62 @@ import (
 )
 
 func TestIntegration_ListMeasurementChanges(t *testing.T) {
-	g := NewWithT(t)
-
 	client := sumoapi.New()
 
-	resp, err := client.ListMeasurementChanges(context.Background(), sumoapi.ListRikishiChangesRequest{
-		RikishiID: 3081, // Hakuho
-		BashoID: &sumoapi.BashoID{
-			Year:  2021,
-			Month: 3,
-		},
+	t.Run("for rikishi", func(t *testing.T) {
+		g := NewWithT(t)
+
+		resp, err := client.ListMeasurementChanges(context.Background(), sumoapi.ListRikishiChangesRequest{
+			RikishiID: 3081, // Hakuho
+		})
+
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(resp).ToNot(BeNil())
+		g.Expect(resp).To(HaveLen(15))
+
+		expectedBashoID := sumoapi.BashoID{Year: 2021, Month: 3}
+
+		g.Expect(resp[0].ID).To(Equal(sumoapi.RikishiChangeID{BashoID: expectedBashoID, RikishiID: 3081}))
+		g.Expect(resp[0].BashoID).To(Equal(expectedBashoID))
+		g.Expect(resp[0].RikishiID).To(Equal(3081))
+		g.Expect(resp[0].Height).To(Equal(192.0))
+		g.Expect(resp[0].Weight).To(Equal(151.0))
+
+		expectedBashoID = sumoapi.BashoID{Year: 2001, Month: 3}
+
+		g.Expect(resp[14].ID).To(Equal(sumoapi.RikishiChangeID{BashoID: expectedBashoID, RikishiID: 3081}))
+		g.Expect(resp[14].BashoID).To(Equal(expectedBashoID))
+		g.Expect(resp[14].RikishiID).To(Equal(3081))
+		g.Expect(resp[14].Height).To(Equal(180.0))
+		g.Expect(resp[14].Weight).To(Equal(80.0))
 	})
 
-	g.Expect(err).ToNot(HaveOccurred())
-	g.Expect(resp).ToNot(BeNil())
-	g.Expect(len(resp)).To(BeNumerically(">", 1)) // Bug: The bashoId filter is not working.
+	t.Run("for basho", func(t *testing.T) {
+		g := NewWithT(t)
 
-	expectedBashoID := sumoapi.BashoID{Year: 2021, Month: 3}
+		bashoID := sumoapi.BashoID{
+			Year:  2025,
+			Month: 9,
+		}
 
-	hakuho := resp[0]
-	g.Expect(hakuho.ID).To(Equal(sumoapi.RikishiChangeID{BashoID: expectedBashoID, RikishiID: 3081}))
-	g.Expect(hakuho.BashoID).To(Equal(expectedBashoID))
-	g.Expect(hakuho.RikishiID).To(Equal(3081))
-	g.Expect(hakuho.Height).To(Equal(192.0))
-	g.Expect(hakuho.Weight).To(Equal(151.0))
+		resp, err := client.ListMeasurementChanges(context.Background(), sumoapi.ListRikishiChangesRequest{
+			BashoID: &bashoID,
+		})
+
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(resp).ToNot(BeNil())
+		g.Expect(resp).To(HaveLen(4))
+
+		g.Expect(resp[0].ID).To(Equal(sumoapi.RikishiChangeID{BashoID: bashoID, RikishiID: 9098}))
+		g.Expect(resp[0].BashoID).To(Equal(bashoID))
+		g.Expect(resp[0].RikishiID).To(Equal(9098))
+		g.Expect(resp[0].Height).To(Equal(178.0))
+		g.Expect(resp[0].Weight).To(Equal(119.0))
+
+		g.Expect(resp[3].ID).To(Equal(sumoapi.RikishiChangeID{BashoID: bashoID, RikishiID: 9101}))
+		g.Expect(resp[3].BashoID).To(Equal(bashoID))
+		g.Expect(resp[3].RikishiID).To(Equal(9101))
+		g.Expect(resp[3].Height).To(Equal(175.0))
+		g.Expect(resp[3].Weight).To(Equal(117.0))
+	})
 }
